cd5a0cf8cc83ef0e1b2e4da0b311385be3385bfc
gaze9999 <gaze9999@users.noreply.github.com>
2022-09-29 09:46:58 +0000

    deploy: 1e029c63cfb587518ff5df579cfc87a4a2893e44

--- assets/SplatoonView.52d9eed3.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as o,b as t,e as n}from"./index.67f49a60.js";const c={},s=n("h1",null,"Splatoon",-1),a=[s];function _(r,l){return o(),t("div",null,a)}const i=e(c,[["render",_]]);export{i as default};
